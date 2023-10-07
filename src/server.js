9094ef528cbc8f4be0d27eb6486a28f6fb7381e8
tamta123 <tamtag186@gmail.com>
2023-10-07 13:18:10 +0400

    change the teble name

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", async (_, res) => {
   try {
-    const result = await pool.query("SELECT FROM users");
+    const result = await pool.query("SELECT FROM user_choices");
     return res.status(200).json(result.rows);
   } catch (error) {
     return res.status(500).json({ massage: error });
